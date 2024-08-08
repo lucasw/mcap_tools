54cf791fa45e7e55309ca2e841af5dec397b6904
Lucas Walter <wsacul@gmail.com>
2024-08-08 08:48:26 -0700

    now have schemas in hashmap so can reuse them between channels

--- mcap_tools/src/mcap_record.rs
+++ mcap_tools/src/mcap_record.rs
@@ -34,6 +34,8 @@ async fn main() -> Result<(), anyhow::Error> {
     let mcap_name = "out.mcap";
     let mcap_out = mcap::Writer::new(BufWriter::new(fs::File::create(mcap_name)?))?;
     let mcap_out = Arc::new(Mutex::new(mcap_out));
+
+    let schemas = Arc::new(Mutex::new(HashMap::new()));
     /*
     let mut writer = mcap::WriteOptions::new()
         .compression(None)
@@ -57,88 +59,95 @@ async fn main() -> Result<(), anyhow::Error> {
         // TODO(lucasw) optionally limit to namespace of this node (once this node can be launched into
         // a namespace)
         let topics = master_client.get_published_topics(ns.clone()).await?;
-        let mut cur_topics = HashSet::<(String, String)>::new();
-        for (topic_name, topic_type) in &topics {
-            cur_topics.insert(((*topic_name).clone(), (*topic_type).clone()));
-            // log::debug!("{topic_name} - {topic_type}");
-        }
+        let cur_topics = {
+            let mut cur_topics = HashSet::<(String, String)>::new();
+            for (topic_name, topic_type) in &topics {
+                cur_topics.insert(((*topic_name).clone(), (*topic_type).clone()));
+                // log::debug!("{topic_name} - {topic_type}");
+            }
+            cur_topics
+        };
 
         for topic_and_type in &old_topics {
             if !cur_topics.contains(topic_and_type) {
                 log::debug!("removed {topic_and_type:?}");
             }
         }
+
         for topic_and_type in &cur_topics {
-            if !old_topics.contains(topic_and_type) {
-                log::debug!("added {topic_and_type:?}");
-                let (topic, topic_type) = topic_and_type;
-                let topic_copy = topic.clone();
-                // TODO(lucasw) the type is almost certainly not std_msgs::ByteMultiArray,
-                // but need to provide some type to downstream machinery
-                let mut subscriber = nh.subscribe_any::<std_msgs::ByteMultiArray>(topic, topic_type, 10).await?;
+            if old_topics.contains(topic_and_type) {
+                continue;
+            }
 
-                // maybe should do arc mutex
-                let nh_copy = nh.clone();
-                let mcap_out = mcap_out.clone();
+            log::debug!("added {topic_and_type:?}");
+            let (topic, topic_type) = topic_and_type.clone();
+            // TODO(lucasw) the type is almost certainly not std_msgs::ByteMultiArray,
+            // but need to provide some type to downstream machinery
+            let mut subscriber = nh.subscribe_any::<std_msgs::ByteMultiArray>(&topic, &topic_type, 10).await?;
 
-                let _sub = tokio::spawn(async move {
-                    let mut sequence = 0;
-                    // TODO(lucasw) make these Option?
-                    let mut have_definition = false;
-                    let mut channel_id = 0;
-                    // TODO(lucasw) seeing some message arrive repeatedly, but only if the
-                    // publisher starts after this node does?
-                    while let Some(data) = subscriber.next_raw().await {
-                        if let Ok(data) = data {
-                            log::debug!("Got raw message data: {} bytes, {:?}", data.len(), data);
-                            // log::debug("{:}",
-                            if !have_definition {
+            // maybe should do arc mutex
+            let nh_copy = nh.clone();
+            let mcap_out = mcap_out.clone();
+
+            let schemas_copy = schemas.clone();
+            let _sub = tokio::spawn(async move {
+                let mut sequence = 0;
+                let mut channel_id = None;
+                // TODO(lucasw) seeing some message arrive repeatedly, but only if the
+                // publisher starts after this node does?
+                while let Some(data) = subscriber.next_raw().await {
+                    if let Ok(data) = data {
+                        log::debug!("Got raw message data: {} bytes, {:?}", data.len(), data);
+                        if channel_id.is_none() {
+                            if !schemas_copy.lock().unwrap().contains_key(&topic_type.clone()) {
                                 log::debug!("get definition");
-                                let definition = nh_copy.inner.get_definition(topic_copy.clone()).await;
-                                if let Ok(definition) = definition {
-                                    have_definition = true;
-                                    log::debug!("definition: '{definition}'");
-                                    // TODO(lucasw) make hashmap of schemas for this type and reuse
-                                    // them?
-                                    let schema = mcap::Schema {
-                                        name: topic_copy.clone(),
-                                        encoding: "ros1msg".to_string(),
-                                        // TODO(lucasw) definition doesn't live long enough
-                                        data: Cow::from(definition.as_bytes().to_owned()),
-                                    };
-                                    let channel = mcap::Channel {
-                                        topic: topic_copy.clone(),
-                                        schema: Some(std::sync::Arc::new(schema.to_owned())),
-                                        message_encoding: "ros1msg".to_string(),
-                                        metadata: BTreeMap::default(),
-                                    };
-                                    // TODO(lucasw) how to do ? in async block?
-                                    channel_id = mcap_out.lock().unwrap().add_channel(&channel).unwrap();
-                                }
+                                let definition = nh_copy.inner.get_definition(topic.clone()).await.unwrap();
+                                log::debug!("definition: '{definition}'");
+                                let schema = mcap::Schema {
+                                    name: topic.clone(),
+                                    encoding: "ros1msg".to_string(),
+                                    // TODO(lucasw) definition doesn't live long enough
+                                    data: Cow::from(definition.as_bytes().to_owned()),
+                                };
+                                let schema = Some(Arc::new(schema.to_owned()));
+                                schemas_copy.lock().unwrap().insert(topic_type.to_owned().to_string(), schema);
+                                // log::warn!("can't get definition {topic} {topic_type}");
                             }
-                            // TODO(lucasw) u64 will only get us to the year 2554...
-                            let ns_epoch = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos() as u64;
-                            mcap_out.lock().unwrap().write_to_known_channel(
-                                &mcap::records::MessageHeader {
-                                    channel_id,
-                                    sequence,
-                                    log_time: ns_epoch,
-                                    // TODO(lucasw) get this from somewhere
-                                    publish_time: ns_epoch,
-                                },
-                                &*data,
-                            ).unwrap();
-                            sequence += 1;
-                        } else {
-                            log::error!("{data:?}");
+                            let schema = schemas_copy.lock().unwrap().get(&topic_type).unwrap().clone();
+
+                            let channel = mcap::Channel {
+                                topic: topic.clone(),
+                                schema: schema,
+                                message_encoding: "ros1msg".to_string(),
+                                metadata: BTreeMap::default(),
+                            };
+
+                            channel_id = Some(mcap_out.lock().unwrap().add_channel(&channel).unwrap());
                         }
-                    }  // get new messages in loop
-                });  // subscriber
-            }  // new topic
+
+                        // TODO(lucasw) u64 will only get us to the year 2554...
+                        let ns_epoch = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos() as u64;
+                        mcap_out.lock().unwrap().write_to_known_channel(
+                            &mcap::records::MessageHeader {
+                                channel_id: channel_id.unwrap(),
+                                sequence,
+                                log_time: ns_epoch,
+                                // TODO(lucasw) get this from somewhere
+                                publish_time: ns_epoch,
+                            },
+                            &*data,
+                        ).unwrap();
+
+                        sequence += 1;
+                    } else {
+                        log::error!("{data:?}");
+                    }
+                }  // get new messages in loop
+            });  // subscriber
         }  // loop through current topics
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 
-        old_topics = cur_topics;
+        old_topics = cur_topics.clone().to_owned();
     }
 
     // Ok(())
